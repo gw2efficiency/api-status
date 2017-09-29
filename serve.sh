cda3967d633151a9ef055366290e9ec0789abcf2
queicherius <queicherius@gmail.com>
2017-09-30 00:07:19 +0100

    Fix server setup script

--- serve.sh
+++ serve.sh
@@ -2,13 +2,13 @@
 
 echo "Installing dependencies..."
 # npm install -g pm2@2.7.1
-cd frontend && yarn
-cd server && yarn
-cd worker && yarn
+cd frontend && yarn && cd ..
+cd server && yarn && cd ..
+cd worker && yarn && cd ..
 
 echo "Bundling frontend..."
-cd frontend && npm run build
+cd frontend && npm run build && cd ..
 
 echo "Starting servers..."
-pm2 start ./process.yml
-# pm2 startup
+pm2 start process.yml
+pm2 startup
