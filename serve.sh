104d92c31748f32f6ee6525e3080e6f9558d2e1c
queicherius <queicherius@gmail.com>
2017-09-30 17:18:29 +0100

    Update the serve script

--- serve.sh
+++ serve.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-echo "Installing dependencies..."
+# - Setup NGINX reverse proxy
+# https://github.com/queicherius/braindump/blob/master/docs/https-and-caching-for-nodejs.md
+# - Run manually:
 # npm install -g pm2@2.7.1
+# pm2 startup
+
+echo "Pulling changes..."
+git pull
+
+echo "Installing dependencies..."
 yarn
 
 echo "Bundling frontend..."
@@ -9,4 +17,4 @@ cd frontend && npm run build && cd ..
 
 echo "Starting servers..."
 pm2 start process.yml
-pm2 startup
+pm2 dump
