35c119fb9687151dcf9898332f0aa7292bb0c453
queicherius <queicherius@gmail.com>
2020-09-30 22:26:29 +0000

    Make the logs a little lesss massive

--- worker/src/runTests.js
+++ worker/src/runTests.js
@@ -4,7 +4,6 @@ const endpoints = require('./endpoints')
 async function runTests () {
   console.log('='.repeat(50))
   console.log(`> Testing ${endpoints.length} API endpoints (${new Date().toISOString()})`)
-  console.log()
 
   let results = []
 
@@ -13,22 +12,17 @@ async function runTests () {
   }
 
   let possiblyBroken = results.filter(x => x.schemaValid === false)
-  let definitelyBroken = results.filter(x => x.status !== 200)
+  let definitelyBroken = results.filter(x => x.status >= 400)
 
   if (definitelyBroken.length > 0) {
     console.log(`❗ ERROR: ${definitelyBroken.length} API endpoint(s)`)
-    console.log(definitelyBroken)
-    console.log()
   }
 
   if (possiblyBroken.length > 0) {
     console.log(`⚠️ WARNING: ${possiblyBroken.length} API endpoint(s)`)
-    console.log(possiblyBroken)
-    console.log()
   }
 
   console.log(`✔️ OK: ${results.length - possiblyBroken.length - definitelyBroken.length} API endpoint(s)`)
-  console.log(`✔️ OK: ${results.filter(x => x.schemaValid).length} matching schemas`)
   console.log('='.repeat(50))
   console.log()
 
